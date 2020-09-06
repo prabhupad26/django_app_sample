dc4a399d10efef91845facac864806cb88df3e9d
prabhupad26 <prabhupad26@gmail.com>
2020-09-06 15:55:57 +0530

    Removed cache files

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.utils import timezone
+import datetime
+from .models import Question
 
-# Create your tests here.
+
+class QuestionModelTests(TestCase):
+    def future_polls(self):
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
